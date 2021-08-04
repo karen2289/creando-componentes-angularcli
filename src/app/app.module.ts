01c19e21c0db9742261d0e0dcd3f3d9f75387298
Karen Aguirre <latingirl_2289@hotmail.com>
2021-08-04 12:15:29 +0200

    Creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { TablaComponent } from './body/tabla/tabla.component';
+import { ListaComponent } from './body/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BodyComponent,
+    TablaComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
